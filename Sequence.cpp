0345bc3329c8aedb4696ac33988a4be354f4748d
grosen <rosen.9@wright.edu>
2025-10-03 00:38:21 -0400

    Skeleton of Sequence class and header finished.

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,1 +1,126 @@
-// Comment here to make change to Sequence.cpp to test commit process from within CLion
+#include "Sequence.h"
+
+// Creates an empty sequence (numElts == 0) or a sequence of numElts items
+// indexed from 0 ... (numElts - 1).
+Sequence::Sequence(size_t sz) {
+
+}
+
+// Creates a (deep) copy of sequence s
+Sequence::Sequence(const Sequence& s) {
+
+}
+// Destroys all items in the sequence and release the memory
+// associated with the sequence
+Sequence::~Sequence() {
+
+}
+
+// The current sequence is released and replaced by a (deep) copy of sequence
+// s. A reference to the copied sequence is returned (return *this;).
+Sequence& Sequence::operator=(const Sequence& s) {
+    Sequence outSeq = Sequence();
+    return outSeq;
+}
+
+
+// The position satisfies ( position >= 0 && position <= last_index() ).
+// The return value is a reference to the item at index position in the
+// sequence. Throws an exception if the position is outside the bounds
+// of the sequence
+std::string& Sequence::operator[](size_t position) {
+    std::string outStr = "";
+    return outStr;
+}
+
+// The value of item is append to the sequence.
+void Sequence::push_back(std::string item) {
+
+}
+
+// The item at the end of the sequence is deleted and size of the sequence is
+// reduced by one. If sequence was empty, throws an exception
+void Sequence::pop_back() {
+
+}
+
+// The position satisfies ( position >= 0 && position <= last_index() ). The
+// value of item is inserted at position and the size of sequence is increased
+// by one. Throws an exceptionif the position is outside the bounds of the
+// sequence
+void Sequence::insert(size_t position, std::string item) {
+
+}
+
+// Returns the first element in the sequence. If the sequence is empty, throw an
+// exception.
+std::string Sequence::front() const {
+    if (head != nullptr) {
+        return head->item;
+    }
+    else {
+        return "";
+    }
+}
+
+// Return the last element in the sequence. If the sequence is empty, throw an
+// exception.
+std::string Sequence::back() const {
+    if (head != nullptr) {
+        return tail->item;
+    }
+    else {
+        return "";
+    }
+}
+
+// Return true if the sequence has no elements, otherwise false.
+bool Sequence::empty() const {
+    if (head == nullptr) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+// Return the number of elements in the sequence.
+size_t Sequence::size() const {
+    return length;
+}
+
+// All items in the sequence are deleted and the memory associated with the
+// sequence is released, resetting the sequence to an empty state that can have
+// items re-inserted.
+void Sequence::clear() {
+
+}
+
+// The item at position is removed from the sequence, and the memory
+// is released. If called with an invalid position throws an exception.
+void Sequence::erase(size_t position) {
+
+}
+
+// The items in the sequence at ( position ... (position + count - 1) ) are
+// deleted and their memory released. If called with invalid position and/or
+// count throws an exception.
+void Sequence::erase(size_t position, size_t count) {
+
+}
+
+// Outputs all elements (ex: <4, 8, 15, 16, 23, 42>) as a string to the output
+// stream. This is *not* a method of the Sequence class, but instead it is a
+// friend function
+std::ostream& operator<<(std::ostream& os, const Sequence& s) {
+
+}
+
+//default constructor, ensure next and prev are nullptr
+SequenceNode::SequenceNode() : next(nullptr), prev(nullptr)
+{}
+
+// parameterized constructor, next and prev are set to nullptr and the
+// node's element is set to the given value
+SequenceNode::SequenceNode(std::string item) : next(nullptr), prev(nullptr), item(item)
+{}
--- Sequence.h
+++ Sequence.h
@@ -1,1 +1,50 @@
-// Comment here to make change to Sequence.h to test commit process from within CLion
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+
+#include <string>
+#include <iostream>
+
+class SequenceNode;
+
+class Sequence {
+    private:
+        SequenceNode* head;
+        SequenceNode* tail;
+        size_t length;
+
+    public:
+        Sequence(size_t sz = 0); // Constructor (If sz parameter is omitted, also acts as default constructor).
+        Sequence(const Sequence& s); // Copy Constructor
+
+        ~Sequence(); // Destructor
+
+        Sequence& operator=(const Sequence& s); // Assignment Operator Overload
+        std::string& operator[](size_t position); // Subscript Operator Overload
+        friend std::ostream& operator<<(std::ostream& os, const Sequence& s); // Stream Insertion Operator Overload
+
+        std::string front() const; // Getter for item of first node in Sequence
+        std::string back() const; // Getter for item of last node in Sequence
+        size_t size() const; // Getter for length
+
+        bool empty() const; // Predicate for whether Sequence is empty
+
+        void push_back(std::string item); // Append node with item to end of Sequence
+        void insert(size_t position, std::string item); // Insert node with item at position
+
+        void pop_back(); // Remove node at end of Sequence
+        void erase(size_t position); // Removes node at position
+        void erase(size_t position, size_t count); // Removes count sequential nodes starting at position
+        void clear(); // Removes all nodes, leaving Sequence empty
+
+};
+
+class SequenceNode {
+    public: 
+        SequenceNode* next; // pointer to next Node.
+        SequenceNode* prev; // pointer to previous Node.
+        std::string item; // the element being stored in the node
+        SequenceNode();
+        SequenceNode(std::string item);
+};
+
+#endif // SEQUENCE_H
